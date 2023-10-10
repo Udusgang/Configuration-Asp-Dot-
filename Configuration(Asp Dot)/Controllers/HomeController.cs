5efbac97f3843b72fc2031120f812f1d301894be
LAPTOP-MNFSFPHS\User <bhattaraisankalpa47@gmail.com>
2023-10-10 21:38:59 +0545

    Configuration for Controller added

---
+++ Configuration(Asp Dot)/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Configuration_Asp_Dot_.Controllers
+{
+    public class HomeController : Controller
+    {   private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+              _configuration = configuration;
+            //Dependecncy Injection using Default Container
+        }
+        public IActionResult Index()
+        {
+            string testvalue=_configuration.GetValue<string>("test");
+           /* Externally we can pass those value to ViewBag for rendering.*/
+            return View();
+        }
+    }
+}
