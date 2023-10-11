6e67114f30d273eb5316ddf7ee8c8f4d435e3c81
LAPTOP-MNFSFPHS\User <bhattaraisankalpa47@gmail.com>
2023-10-11 08:23:52 +0545

    COmmand for Configuration Environment added

--- Configuration(Asp Dot)/Program.cs
+++ Configuration(Asp Dot)/Program.cs
@@ -1,6 +1,8 @@
+using Configuration_Asp_Dot_;
 using Microsoft.AspNetCore.Http;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<ParentOptions>(builder.Configuration.GetSection("parent"));
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
@@ -13,7 +15,7 @@ app.UseEndpoints(endpoints =>
         await context.Response.WriteAsync(app.Configuration["test"]);//directly 
         await context.Response.WriteAsync(app.Configuration.GetValue<string>("test") + "\n");//using getvalue
         await context.Response.WriteAsync(app.Configuration.GetValue<string>("test","default one") + "\n");//if test not found default is used
-
+        /* $ENV: Parent__Child2 = "Value from Child2"*/    //for config key from environment
     });
 });
 
